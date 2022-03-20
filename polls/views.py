6541c2e7417dd82f2c11b637fdccdc979c701636
gutemberg <gutembergdomingos13@gmail.com>
2022-03-20 20:56:13 -0300

    add news views and urls

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,20 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse('Hello, devs sem camisa, Você está dentro de polls index')
+
+def detail(request, question_id):
+    response = 'Você está buscando a pesgunta %s.'
+    return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    response = 'Você está olhando o resultado da pergunta %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = 'Você está votando na pergunta %s.'
+    return HttpResponse(response % question_id)
 
